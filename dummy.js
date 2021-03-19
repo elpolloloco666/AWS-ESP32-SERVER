a896e7f7216e54ca43a063e4bed1214753d1991a
elpolloloco666 <55962892+elpolloloco666@users.noreply.github.com>
2021-03-18 20:07:21 -0600

    Add files via upload

---
+++ dummy.js
@@ -0,0 +1,29 @@
+const numeros = [
+    {
+        id: 0,
+        nombre: 'Fulanito',
+        clave: 'caaammmmffffC'
+    }, 
+        
+    {
+        id: 20,
+        nombre: 'Chochos',
+        clave: 'zpfg'
+    }, 
+    
+    {
+        id: 1,
+        nombre: 'María',
+        clave: 'lllld'
+    },
+
+    {
+        id: 2,
+        nombre: 'Poio Loko',
+        clave: 'abc'
+    }
+
+
+];
+
+module.exports = numeros; 
